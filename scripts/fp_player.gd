48b89be9e23c323e9bababa153495659d3806118
Peter Gilbert <petegilb@bu.edu>
2023-10-01 21:36:30 -0700

    add clock function to main

--- scripts/fp_player.gd
+++ scripts/fp_player.gd
@@ -70,7 +70,7 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
