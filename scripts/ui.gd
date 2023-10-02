c24898d1c6ecb76ee7e14dd5e1fc183e8d99dff1
Peter Gilbert <petegilb@bu.edu>
2023-10-01 23:22:18 -0700

    add working clock ui

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 class_name UI
 
 @onready var interact_prompt = $Control/MarginContainer/VBoxContainer/InteractPrompt
+@onready var time_label = $Control/TimeMargin/TimeLabel
 
 func _on_interact_prompt_changed(new_value):
 	interact_prompt.text = new_value
+
+func _on_cubicle_game_day_clock_updated(new_time):
+	time_label.text = new_time
