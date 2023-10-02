4770ccba8f7d995a57732f16c02760dc8d264995
Peter Gilbert <petegilb@bu.edu>
2023-10-01 23:34:30 -0700

    interacting with computer now shows email ui

--- scripts/interactables/Computer.gd
+++ scripts/interactables/Computer.gd
@@ -3,5 +3,7 @@ extends Interactable
 func get_prompt():
 	return 'Press %s to login to your computer' % InputMap.action_get_events('action_interact')[0].as_text()
 
-func interact(callback: Callable):
+func interact(interactor: CharacterBody3D, callback: Callable):
 	callback.call("Successfully interacted with the computer")
+	var ui = interactor.get_ui()
+	ui.show_emails()
